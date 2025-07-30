da21648ae70645aa589c59c1d861234e4a159843
Eduardo Kraus <kraus@eduardokraus.com>
2025-07-29 23:07:49 -0300

    V 9.0.0

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025071800;
+$plugin->version = 2025072900;
 $plugin->requires = 2022000000;
-$plugin->release = "8.3.16";
+$plugin->release = "9.0.0";
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'theme_eadflix';
 $plugin->dependencies = [
