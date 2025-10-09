df6528d8bd84a344ef73ebff04a7fe477f03d18f
Eduardo Kraus <kraus@eduardokraus.com>
2025-10-09 15:14:26 -0300

    V 9.2.4

--- version.php
+++ version.php
@@ -25,12 +25,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025100800;
+$plugin->version = 2025100900;
 $plugin->requires = 2022010100;
-$plugin->release = "9.2.2";
+$plugin->release = "9.2.4";
 $plugin->maturity = MATURITY_STABLE;
-$plugin->component = 'theme_eadflix';
+$plugin->component = "theme_eadflix";
 $plugin->dependencies = [
-    "customfield_picture" => 2024100800,
     "theme_eadtraining" => 2025091801,
 ];
