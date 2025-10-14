759322d174abbaa20ba5e01d943ffead4c81c92b
Eduardo Kraus <kraus@eduardokraus.com>
2025-10-14 15:04:49 -0300

    missing setting_scss

--- settings/advanced.php
+++ settings/advanced.php
@@ -22,6 +22,8 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+use theme_eadtraining\admin\setting_scss;
+
 defined('MOODLE_INTERNAL') || die;
 
 // Advanced settings.
