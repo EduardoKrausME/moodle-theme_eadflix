69f3aa69e1d454801c01e4de282472f45841007b
Eduardo Kraus <kraus@eduardokraus.com>
2026-04-14 02:02:23 -0300

    +__DIR__

--- renderers.php
+++ renderers.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-require_once("../eadtraining/renderers.php");
+require_once(__DIR__."/../eadtraining/renderers.php");
 
 /**
  * Class theme_eadflix_core_course_management_renderer
